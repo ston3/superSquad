d7ad048bdc4cbd23bb2d81915a9ce4c971e0b3bd
rainer flores <rainer.flores64@gmail.com>
2017-06-27 22:40:20 -0400

    inicio

---
+++ src/components/App.js
@@ -0,0 +1,14 @@
+import React, {Component} from 'react';
+
+class App extends Component {
+    render(){
+        return(
+            <div>
+                <h1>SuperSquad</h1>
+                <h2>Hello Stone!</h2>
+            </div>
+        )
+    }
+}
+
+export default App;
