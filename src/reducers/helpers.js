0d03c931a84aeb02f3f86389833c1fb28ec03fce
rainer flores <rainer.flores64@gmail.com>
2017-07-05 17:39:34 -0400

    Updates

--- src/reducers/helpers.js
+++
@@ -1,6 +0,0 @@
-import characters_json from '../data/characters.json';
-
-export function createCharacter(id) {
-    let character = characters_json.find(cha => cha.id === id);
-    return character; 
-}
